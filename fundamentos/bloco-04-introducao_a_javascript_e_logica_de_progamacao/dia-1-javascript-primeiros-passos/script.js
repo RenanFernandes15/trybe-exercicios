3d3f531327cd6a790740b94bf5ece470ce4bf670
Renan Fernandes <renzinestuods@gmail.com>
2022-05-13 11:18:06 -0300

    exercicio-2 faz um progama retorna o maior entre 2 numeros

--- fundamentos/bloco-04-introducao_a_javascript_e_logica_de_progamacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao_a_javascript_e_logica_de_progamacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,14 +1,28 @@
-const a = 12
-const b = 4
+// exercicio-1
 
-let adicao = a + b;
-let subtracao = a - b;
-let multiplicacao = a * b;
-let divisao = a / b;
-let modulo = a % b;
+// const a = 12
+// const b = 4
+
+// let adicao = a + b;
+// let subtracao = a - b;
+// let multiplicacao = a * b;
+// let divisao = a / b;
+// let modulo = a % b;
 
 // console.log("Adição =", adicao);
 // console.log("Subtração =", subtracao);
 // console.log("Multiplicação =", multiplicacao);
 // console.log("Divisão", divisao);
 // console.log("Módulo", modulo);
+
+// exercicio-2
+ 
+const c = 36;
+const d = 13;
+
+if (d > c) {
+  console.log(d, "é maior que", c);
+}
+else {
+  console.log(c, "é maior que", d);
+}

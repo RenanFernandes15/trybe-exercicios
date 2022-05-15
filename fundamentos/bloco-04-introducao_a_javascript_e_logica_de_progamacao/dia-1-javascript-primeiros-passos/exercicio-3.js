e4d15d9fcd35c894932644331194569f567f881f
Renan Fernandes <renzinestuods@gmail.com>
2022-05-15 18:19:30 -0300

    exercicio-3 um progama que retorna o maior valor de tres numeros diferentes

---
+++ fundamentos/bloco-04-introducao_a_javascript_e_logica_de_progamacao/dia-1-javascript-primeiros-passos/exercicio-3.js
@@ -0,0 +1,13 @@
+const a = 12
+const b = 4
+const c = 36;
+
+if (a > b && a > c) {
+  console.log("Maior valor é:", a);
+}
+else if (b > a && b > c) {
+  console.log("Maior valor é:", b);
+}
+else {
+  console.log("Maior valor é:", c);
+}

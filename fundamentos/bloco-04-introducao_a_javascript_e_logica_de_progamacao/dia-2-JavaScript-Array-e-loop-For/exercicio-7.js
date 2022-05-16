f9845bba0d2ed37d3c0b91d4451ef68613d1e250
Renan Fernandes <renzinestuods@gmail.com>
2022-05-16 14:53:04 -0300

    exercicio-7 faz algumas modificações no cofigo para ficar mais simple e eficiente

--- fundamentos/bloco-04-introducao_a_javascript_e_logica_de_progamacao/dia-2-JavaScript-Array-e-loop-For/exercicio-7.js
+++ fundamentos/bloco-04-introducao_a_javascript_e_logica_de_progamacao/dia-2-JavaScript-Array-e-loop-For/exercicio-7.js
@@ -1,17 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maior= 0;
-let menor;
+let menor = numbers[0];
 
 for(let index = 0; index < numbers.length; index += 1) {
-  if (numbers[index] > maior) {
-    maior = numbers[index];
-  };
-};
-menor = maior;
-
-for (let index = 0; index < numbers.length; index += 1) {
   if (numbers[index] < menor) {
-    menor = numbers[index]
+    menor = numbers[index];
   };
 };
-console.log("Menor valor", menor);
+console.log(menor);

a7f99e7d54d590da50da1a245699018397db4260
Renan Fernandes <renzinestuods@gmail.com>
2022-05-17 19:06:32 -0300

    Exercicio-5 faz um console.log com uma fraze e acessa a chave medals.

--- fundamentos/bloco-04-introducao_a_javascript_e_logica_de_progamacao/dia-4-javascript-objetos-e-funcoes/Para-fixar/exercicios.js
+++ fundamentos/bloco-04-introducao_a_javascript_e_logica_de_progamacao/dia-4-javascript-objetos-e-funcoes/Para-fixar/exercicios.js
@@ -27,6 +27,7 @@ player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
 
 console.log('A jogadora ' + player.name + player.lastName + ' foi eleita a melhor do mundo por 6 vezes. ' + player.bestInTheWorld);
 
-// Exercicio-5 
+// Exercicio-5 faz um console.log com uma fraze e acessa a chave medals.
 
 console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + 'medalhas de prata.');
+

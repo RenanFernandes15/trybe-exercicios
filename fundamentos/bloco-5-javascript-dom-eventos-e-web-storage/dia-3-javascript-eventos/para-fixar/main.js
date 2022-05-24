a4886f4122b1ff880753592253adbf15352f321c
Renan Fernandes <renzinestuods@gmail.com>
2022-05-24 19:31:53 -0300

    Para-fixar exercicio-4: cria uma função que ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele redireciona para uma nova pagina

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/para-fixar/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/para-fixar/main.js
@@ -49,7 +49,7 @@ textbox.addEventListener('input', () => {
 const meuTop = document.querySelector('#my-spotrybefy');
 meuTop.addEventListener('dblclick', () => {
   meuTop = window.open('https://github.com/RenanFernandess', '_blank')
-});
+}); 
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;

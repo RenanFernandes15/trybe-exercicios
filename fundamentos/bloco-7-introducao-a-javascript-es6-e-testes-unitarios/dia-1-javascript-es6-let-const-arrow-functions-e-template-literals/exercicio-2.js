eb65f40801bafe72215c069909e4ed63680a0532
Renan Fernandes <renzinestuods@gmail.com>
2022-06-09 13:01:44 -0300

    Exercicio-2: retorna o array oddsAndEvens em ordem crescente

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-2.js
@@ -0,0 +1,5 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const order = (a, b) => a - b;
+
+console.log(`Os números ${oddsAndEvens.sort(order)} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉

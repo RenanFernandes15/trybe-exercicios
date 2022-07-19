282ceca115d1fe4832385945df6507d3e7eab5fa
Renan Fernandes <renzinestuods@gmail.com>
2022-07-19 11:46:12 -0300

    Exercícios react. Exercise-3: Imporata o componente Header no App.js e renderiza ele

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise-3/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Component } from 'react';
+import Header from './Header';
+
+class App extends Component {
+  render() {
+    return <Header />
+  }
+};
+
+export default App;

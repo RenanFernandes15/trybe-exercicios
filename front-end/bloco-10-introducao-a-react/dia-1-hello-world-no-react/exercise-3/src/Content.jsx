d1f3a048f225fda63a181425f3a9cd122d40d6f8
Renan Fernandes <renzinestuods@gmail.com>
2022-07-19 13:33:16 -0300

    Exercícios react. Exercise-3: Renderiza a lista de conteudos

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise-3/src/Content.jsx
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise-3/src/Content.jsx
@@ -1,8 +1,47 @@
-import React, { Component } from "react";
+import { Component } from "react";
+
+const conteudos = [
+  {
+    conteudo: 'High Order Functions',
+    bloco: 8,
+    status: 'Aprendido'
+  },
+  {
+    conteudo: 'Composicao de Componentes',
+    bloco: 11,
+    status: 'Aprendendo',
+  },
+  {
+    conteudo: 'Composicao de Estados',
+    bloco: 12,
+    status: 'Aprenderei'
+  },
+  {
+    conteudo: 'Redux',
+    bloco: 16,
+    status: 'Aprenderei'
+  },
+];
+
+const createList = ({ conteudo, bloco, status }) => {
+  return (
+    <li>
+      <p>O conteúdo é: { conteudo }</p>
+      <p>Status: { status }</p>
+      <p>Bloco: { bloco }</p>
+    </li>
+  )
+}
+
+const list = conteudos.map((value) => createList(value));
 
 class Content extends Component {
   render() {
-    return 
+    return (
+      <ol>
+        { list }
+      </ol>
+    )
   }
 };
 

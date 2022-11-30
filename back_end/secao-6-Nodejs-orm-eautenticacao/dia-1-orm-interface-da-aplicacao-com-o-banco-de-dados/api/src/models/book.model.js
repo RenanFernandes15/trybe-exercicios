3469c33fcfa7e7f739aa9462803a8a31f1c2e7e6
Renan Fernandes <renzinestuods@gmail.com>
2022-11-30 15:02:53 -0300

    :sparkles: feat: Cria model da tabela Books

---
+++ back_end/secao-6-Nodejs-orm-eautenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/api/src/models/book.model.js
@@ -0,0 +1,16 @@
+const BookModel = (sequelize, DataTypes) => {
+  const Book = sequelize.define('Book', {
+    title: DataTypes.STRING,
+    author: DataTypes.STRING,
+    pageQuantity: DataTypes.INTEGER,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  },
+  {
+    tableName: 'Books',
+  });
+
+  return Book;
+}
+
+module.exports = BookModel;

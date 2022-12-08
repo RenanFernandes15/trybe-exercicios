4f6edd8e18dda5bc426a21a2b6df92b24b60e214
Renan Fernandes <renzinestuods@gmail.com>
2022-12-08 15:50:28 -0300

    :sparkles: feat: Cria a função getRectangleArea que calcula a area de um retamgulo, ela recebe a base e altura do retangulo e retorna o valor da area

--- back_end/secao-8-introducao-a-typescript/dia-1-introducao-a-typescript/primeiro-progama-em-typescript/src/exercises.ts
+++ back_end/secao-8-introducao-a-typescript/dia-1-introducao-a-typescript/primeiro-progama-em-typescript/src/exercises.ts
@@ -1,2 +1,5 @@
+import { isModuleNamespaceObject } from "util/types";
+
 export const getSquareArea = (side: number): number => side ** 2;
 
+export const getRectangleArea = (base: number, heigth: number): number => base * heigth;

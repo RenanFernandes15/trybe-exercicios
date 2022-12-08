a7b6761106907eb2752d3094c58490fc525d11f7
Renan Fernandes <renzinestuods@gmail.com>
2022-12-08 15:53:18 -0300

    :boom: fix: Remove a importação que não estava sendo utilizada

--- back_end/secao-8-introducao-a-typescript/dia-1-introducao-a-typescript/primeiro-progama-em-typescript/src/exercises.ts
+++ back_end/secao-8-introducao-a-typescript/dia-1-introducao-a-typescript/primeiro-progama-em-typescript/src/exercises.ts
@@ -1,5 +1,3 @@
-import { isModuleNamespaceObject } from "util/types";
-
 export const getSquareArea = (side: number): number => side ** 2;
 
 export const getRectangleArea = (base: number, heigth: number): number => base * heigth;

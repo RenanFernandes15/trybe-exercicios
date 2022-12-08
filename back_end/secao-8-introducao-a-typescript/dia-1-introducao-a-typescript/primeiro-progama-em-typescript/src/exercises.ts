d41f2127c9f8f83f4ecb81ca2265ba74d263b83d
Renan Fernandes <renzinestuods@gmail.com>
2022-12-08 16:18:41 -0300

    :sparkles: feat: Cria a função triangleCheck que verifica se é um triangulo, ala recebe as medidas dos lados de um triangulo, caso seja um triangulo retorna 'true', caso contrario 'false'

--- back_end/secao-8-introducao-a-typescript/dia-1-introducao-a-typescript/primeiro-progama-em-typescript/src/exercises.ts
+++ back_end/secao-8-introducao-a-typescript/dia-1-introducao-a-typescript/primeiro-progama-em-typescript/src/exercises.ts
@@ -5,3 +5,10 @@ export const getRectangleArea = (base: number, height: number): number => base *
 export const getTriangleArea = (base: number, height: number): number => (base * height) / 2;
 
 export const getPolygonPerimeter = (sides: number[]): number => sides.reduce((sum, side) => sum + side, 0);
+
+export function triangleCheck(sideA: number, sideB: number, sideC: number): boolean {
+  const checkSideA = (sideB - sideC) < sideA && sideA < (sideB - sideC);
+  const checkSideB = (sideA - sideC) < sideB && sideB < (sideA + sideC);
+  const checkSideC = (sideA - sideB) < sideC && sideC < (sideA + sideC);
+  return checkSideA && checkSideB && checkSideC;
+}

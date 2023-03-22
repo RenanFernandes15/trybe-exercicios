2aad5fcd0897fc9d334c3c6bdcf1263dcac6d27c
Renan Fernandes <renzinestuods@gmail.com>
2023-03-22 20:30:01 -0300

    :sparkles: feat: Cria a função lower_value que dada uma lista, descobre o menor elemento

---
+++ ciencia-da-computacao/secao-1-introducao-python/dia-01-aprendendo-python/bonus-exercises/exercise01.py
@@ -0,0 +1,9 @@
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+
+
+def lower_value(list):
+    """Dada uma lista, descobre o menor elemento"""
+    return min(list)
+
+
+print(lower_value(list=numbers))

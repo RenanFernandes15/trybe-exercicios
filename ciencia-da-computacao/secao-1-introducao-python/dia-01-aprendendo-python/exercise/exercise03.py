48d072c10a6f367e66102f66bf234868d2908fdc
Renan Fernandes <renzinestuods@gmail.com>
2023-03-22 18:51:42 -0300

    :recycle: refactor: Refatora a função print_frame

--- ciencia-da-computacao/secao-1-introducao-python/dia-01-aprendendo-python/exercise/exercise03.py
+++ ciencia-da-computacao/secao-1-introducao-python/dia-01-aprendendo-python/exercise/exercise03.py
@@ -3,7 +3,8 @@ def print_frame(number):
     imprima na tela um quadrado feito de asteriscos de lado de tamanho n"""
     if number > 1:
         for index in range(number):
-            print(" ".join(["*" for index in range(number)]))
+            # print(" ".join(["*" for index in range(number)]))
+            print(number * "* ")
 
 
 print_frame(8)

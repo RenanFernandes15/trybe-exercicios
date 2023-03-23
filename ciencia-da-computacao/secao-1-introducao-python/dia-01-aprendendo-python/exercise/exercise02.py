ac2bf08085ba722930aa203d3e3f397bfb36b2fc
Renan Fernandes <renzinestuods@gmail.com>
2023-03-23 12:29:15 -0300

    :recycle: refactor: Refatora a função calculate_arithmetic_mean, agora utiliza a lib statistics

--- ciencia-da-computacao/secao-1-introducao-python/dia-01-aprendendo-python/exercise/exercise02.py
+++ ciencia-da-computacao/secao-1-introducao-python/dia-01-aprendendo-python/exercise/exercise02.py
@@ -1,9 +1,12 @@
-numbers = [4, 2]
+import statistics
+
+numbers = [4, 2, 3, 1]
 
 
 def calculate_arithmetic_mean(list):
     """Calcula a média aritmética dos valores contidos em uma lista"""
-    return sum(list) / len(list)
+    # return sum(list) / len(list)
+    return statistics.fmean(numbers)
 
 
 print(calculate_arithmetic_mean(numbers))

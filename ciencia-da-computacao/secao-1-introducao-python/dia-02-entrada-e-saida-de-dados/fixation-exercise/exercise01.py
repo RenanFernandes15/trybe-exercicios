aa09d894042ce97542f76ada204f8b61e42ab6c9
Renan Fernandes <renzinestuods@gmail.com>
2023-03-24 11:51:17 -0300

    :sparkles: feat: Cria a função print_name que solicita o nome de uma pessoa usuária e imprima-o na vertical

---
+++ ciencia-da-computacao/secao-1-introducao-python/dia-02-entrada-e-saida-de-dados/fixation-exercise/exercise01.py
@@ -0,0 +1,7 @@
+def print_name():
+    """Solicita o nome de uma pessoa usuária e imprima-o na vertical"""
+    for character in input("Digite seu nome!"):
+        print(character)
+
+
+print_name()
